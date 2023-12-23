eae119102287d96f60357ccf2891287ffeb1ed14
jonathanraya <jonathanraya79@gmail.com>
2023-12-22 18:37:03 -0600

    finished app

--- tic-tac-toe/src/components/Board.js
+++ tic-tac-toe/src/components/Board.js
@@ -2,27 +2,46 @@ import React from 'react'
 import Square from './Square'
 import '../App.css'
 
-    const Board = ({ squares, onClick }) => {
+    const Board = ({ squares, onClick, background }) => {
     const renderSquare = (i) => (
         <Square value={squares[i]} onClick={() => onClick(i)} />
     )
 
+
+    const getBackgroundImage = (background) => {
+        switch (background) {
+            case 'background 1':
+                return 'https://i.postimg.cc/R03YxTgz/trees.jpg'
+            case 'background 2': 
+                return 'https://i.postimg.cc/L8V9ZmSs/Cartoon-Astronaut-Wallpapers-For-Phone-Free-Download-844x1500.jpg'
+            case 'background 3':
+                return 'https://i.postimg.cc/7Ykr1jmJ/rectangle.jpg'
+        }
+    }
+
+    // const boardStyle = {
+    //     background: `url(${getBackgroundImage(background)})`,
+    // }
+
     return (
     <>
-        <div className="board-row">
-            {renderSquare(0)}
-            {renderSquare(1)}
-            {renderSquare(2)}
-        </div>
-        <div className="board-row">
-            {renderSquare(3)}
-            {renderSquare(4)}
-            {renderSquare(5)}
-        </div>
-        <div className="board-row">
-            {renderSquare(6)}
-            {renderSquare(7)}
-            {renderSquare(8)}
+
+        <div className='board-background' style={{ backgroundImage: `url(${getBackgroundImage(background)})`, backgroundSize: '100% 100%', backgroundRepeat: 'no-repeat' }}>
+            <div className="board-row" >
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
+            </div>
+            <div className="board-row">
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+            </div>
+            <div className="board-row">
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+            </div>
         </div>
     </>    
     )
--- tic-tac-toe/src/components/Game.js
+++ tic-tac-toe/src/components/Game.js
@@ -1,13 +1,18 @@
 import React, { useState } from 'react'
 import Board from './Board'
 
-    const Game = () => {
+const Game = () => {
     const [history, setHistory] = useState([{ squares: Array(9).fill(null) }])
     const [stepNumber, setStepNumber] = useState(0)
     const xIsNext = stepNumber % 2 === 0
-
+    const [selectedBackground, setSelectedBackground] = useState('background 1')
     const current = history[stepNumber]
     const winner = calculateWinner(current.squares)
+
+
+    const handleBackgroundChange = (background) => {
+        setSelectedBackground(background)
+    }
 
     const handleClick = (i) => {
         const newHistory = history.slice(0, stepNumber + 1)
@@ -30,21 +35,28 @@ import Board from './Board'
     };
 
     const status = winner
-        ? `Winner: ${winner}`
-        : `Next person: ${xIsNext ? 'X' : 'O'}`
+        ? `The letter ${winner} Wins!`
+        : `Current Letter: ${xIsNext ? 'X' : 'O'}`
 
     return (
         <div className="game">
         <div className="game-board">
-            <Board squares={current.squares} onClick={handleClick} />
+            <Board squares={current.squares} onClick={handleClick} background={selectedBackground}/>
             <div className="game-info">
             <h3>{status}</h3>
-            {<button onClick={handleReset} className="reset-button">
-                Play Again
-                </button>}
             </div>
+            <button onClick={handleReset} className="buttons">
+                Reset Board
+                </button>
+            <span className='spanner'> Select The Board Background  </span>
+            <select className='buttons' onChange={(e) => handleBackgroundChange(e.target.value)}>
+                <option value='background 1'>Trees</option>
+                <option value="background 2">Astronaut</option>
+                <option value="background 3">Leaves</option>
+                
+            </select>
         </div>
-        </div>
+        // </div>
     )
     }
 
